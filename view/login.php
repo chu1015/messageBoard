f5c0148c01d614b27d221d8aba57665304fb2b95
unknown <kuan-chi@bb.com>
2019-11-12 18:43:46 +0800

    191112end

--- view/login.php
+++ view/login.php
@@ -1,39 +1,46 @@
 <?php
 require_once ('sql.php');
-session_start();
-if(isset($_SESSION["member"]) && ($_SESSION["member"] !="")){
-    if($_SESSION["memberLevel"]=="member"){
-        header("Location:index");
-    }
-    // else{
-    //     header("Location:index");
-    // }
-}
+// session_start();
+// if(isset($_SESSION["member"]) && ($_SESSION["member"] !="")){
+//     if($_SESSION["memberLevel"]=="member"){
+//         header("Location:index");
+//     }
+//     // else{
+//     //     header("Location:index");
+//     // }
+// }
 
 if (!isset($_REQUEST['account'])) header('Location:member.html');
-$name = $_REQUEST["name"];
+// $name = $_REQUEST["name"];
 $account = $_REQUEST['account'];
 $password = $_REQUEST['password'];
 
-$sql = "SELECT mName, mPassword, mLevel FROM member WHERE account = ?";
+$sql = "SELECT mName, mPassword, mLevel FROM member WHERE mAccount = ?";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param('s', $account);
 $stmt->execute();
+// echo $stmt->execute();
 
 
 $result = $stmt->get_result();
+// echo $result;
 if ($result->num_rows > 0) {
     $member = $result->fetch_object();
     // $member->account . '<br>' . $member->password
     if (password_verify($password, $member->mPassword)) {
-        $_SESSION['member'] = $member->mName;
-        $_SESSION['memberLevel'] = $member;
-        header('Location:index.php');
+        // $_SESSION['member'] = $member->mName;
+        // $_SESSION['memberLevel'] = $member->mLevel;
+        echo "success";
+        // header('Location:index.html');
     } else {
+        echo "fail";
+        // echo "<script>console.log('$account,$password')</script>";
         header('Location:member.html');
     }
 } else {
     //not exist account
-    header('Location: lmember.html');
+    echo "error";
+    header('Location: member.html');
 }
+
 ?>
